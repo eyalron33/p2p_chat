aacd9fd9295d9118030c5b3aa2a7478f8447027a
eyalron33 <eyalron33@gmail.com>
2017-09-01 17:04:32 +0200

    add execute intructions in p2p-chat.cpp

--- p2p-chat.cpp
+++ p2p-chat.cpp
@@ -9,6 +9,9 @@
  *    2. Put the following files into the 'object' folder: 
  *          libboost_date_time.a, libboost_random.a, libboost_system.a, libsioclient.a, libsioclient_tls.a
  *    3. build libnice (https://nice.freedesktop.org/wiki/)
+ *    4. 4. execute: g++ -g -I ./header p2p-chat.cpp 
+ *             object/libsioclient.a object/libboost_date_time.a object/libboost_random.a object/libboost_system.a object/libsioclient_tls.a 
+ *             -o p2p-chat -std=gnu++11 `pkg-config --cflags --libs nice`
  *
  * Run:
  *    p2p-chat <nickname> $(host -4 -t A stun.stunprotocol.org | awk '{ print $4 }')
